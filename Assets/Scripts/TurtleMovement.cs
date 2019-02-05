d8cb4fe94b7ad07b4b5039b83eeed2bb5fbcd504
Max Hemberg <max.hemberg@gmail.com>
2019-02-05 14:09:23 +0100

    Initial commit

---
+++ Assets/Scripts/TurtleMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleMovement : MonoBehaviour {
+
+    public float turtleSpeed;
+    
+
+    float direction = 1; //Start Moving the Turtle Up
+    int topBorder = 4;
+    int botBorder = -4;
+    float unitID;
+    public float frequency;
+
+
+    // Use this for initialization
+    void Start () {
+       unitID = Random.Range(1, 10);
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (transform.position.y >= topBorder) {
+            direction = -1;
+        }
+
+        else if (transform.position.y <= botBorder) {
+            direction = 1;
+        } 
+        else {
+            //Use Sinus Curve to Shift the direction of the Enemies with the offset of unitID
+            direction = Mathf.Sin((Time.time + unitID) * frequency);
+        }
+
+        transform.Translate(0, 0.5f * turtleSpeed * direction * Time.deltaTime, 0);
+	}
+}
---
+++ Assets/Scripts/TurtleShooting.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleShooting : MonoBehaviour {
+
+    float timerFireRate;
+    
+    public GameObject laserBeam;
+    public Transform gunBarrelEnd;
+
+    public float timeBetweenShots;
+    public float safeToFire;
+
+    bool friendlyFire = false;
+
+	// Use this for initialization
+	void Awake () {     
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timerFireRate += Time.deltaTime;              
+                
+        if (timerFireRate >= timeBetweenShots && Time.timeScale != 0 && !friendlyFire) {
+
+            if (timerFireRate >= safeToFire) {
+            Shoot();
+            }
+        }       
+
+    }
+
+    void FixedUpdate()
+    {
+       CheckFriendlyFire();
+
+    }
+
+    void CheckFriendlyFire()
+    {
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left);
+
+        if (hit != false) {
+            if (hit.collider.gameObject.tag == "Enemy") 
+            {
+            timerFireRate = 0;
+            friendlyFire = true;
+            //Debug Friendly Fire
+            print("Friendly Fire!!");
+            } 
+        }
+        else 
+        {                
+        friendlyFire = false;
+        //Debug Friendly Fire
+        print("Fire At Will!");                
+        }
+    }
+
+    void Shoot()
+    {
+        timerFireRate = 0;
+
+       // Spawn Noob Tube shells           
+        Instantiate(laserBeam, gunBarrelEnd.position, gunBarrelEnd.rotation);
+
+    }
+}
