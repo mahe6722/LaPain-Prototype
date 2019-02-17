c573b5e6ec9ce1e5b29c4d8795a210faa4e2bc79
Unohu <Unohu@users.noreply.github.com>
2019-02-17 21:41:44 +0100

    ExtendableArm + MyScene

--- Assets/Scripts/extArm.cs
+++ Assets/Scripts/extArm.cs
@@ -2,28 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class extArm : MonoBehaviour
+public class ExtArm : MonoBehaviour
 {
-    private bool isActive = false;
-    private float maxDistance = 1.9f;    // maxDistance of extArm on x Axis in 1.9
-
+    public float armReach;//5
     public float speed;
 
-    private Rigidbody2D rigidBody;
+    //public LayerMask whatIsSolid;
+
+    SnekoMovement snekoMovement;
+
+    GameObject player;
+
+    private int borderReset;
+
+    private float distance = 0.1f;
+
+    private bool extending = false;
+    private bool returning = false;
+    private bool isActive = false;
+
+    private Vector2 origLocation;
+
 
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody2D>();
-        Vector3 startPos = GameObject.Find("Player").transform.position;
+        player = GameObject.Find("Player");
+        snekoMovement = GameObject.Find("Sneko").GetComponent<SnekoMovement>();
+        borderReset = snekoMovement.currentBorder;
     }
 
-    // Update is called once per frame
-    void Update ()
+    void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isActive == false)
+        if (extending && gameObject.transform.position.x < armReach)
+        {
+            gameObject.transform.Translate(Vector2.right * speed * Time.deltaTime);
+            RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.right, distance);
+            if (hitInfo.collider != null)
+            {
+                if (hitInfo.collider.CompareTag("Carrot"))
+                {
+                    //Accessing the movement script for Sneko. This tells Sneko to retreat because the arm would reach his carrot.
+                    snekoMovement.direction = 1;
+                    snekoMovement.currentBorder = borderReset;
+                    //Counting how many times Spitfire almost caught the carrot with "stageCounter". After 3 "Close Calls" Sneko will change his behavior. Boss fight is not imminent. 
+                    snekoMovement.stageCounter++;
+                    extending = false;
+                    returning = true;
+                }
+            }
+
+        if (gameObject.transform.position.x >= armReach)
+            {
+                extending = false;
+                returning = true;
+            }
+        }
+
+        if (returning)
+        {
+            gameObject.transform.Translate(Vector2.left * speed * Time.deltaTime);
+            if (Vector2.Distance(gameObject.transform.position, origLocation) < 0.3)
+            {
+                returning = false;
+                isActive = false;
+                gameObject.transform.parent = player.transform;
+                player.GetComponent<PlayerMovement>().enabled = true;
+                gameObject.transform.position = origLocation;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isActive == false && extending == false && returning == false && Input.GetKeyUp(KeyCode.Space) && player.GetComponent<Rigidbody2D>().velocity.magnitude <= 0)
         {
             isActive = true;
-            transform.position = transform.forward * speed;
+            extending = true;
+            DetachFromParent();
+            player.GetComponent<PlayerMovement>().enabled = false;
+            //Vector2 origLocation = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+            origLocation = gameObject.transform.position;
         }
-	}
+    }
+
+    public void DetachFromParent()
+    {
+        // Detaches the transform from its parent.
+        transform.parent = null;
+    }
 }
