5b95f661a24fb1b40e154c2e8846b8e90b875788
BjorneJosefsson <ludden1996@outlook.com>
2019-02-27 14:49:41 +0100

    123

---
+++ Assets/Audio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class sound : MonoBehaviour
+{
+
+
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+
+            this.GetComponent<AudioSource>().Play();
+            Debug.Log("PLayed Sound!");
+
+        }
+    }
+
+}
