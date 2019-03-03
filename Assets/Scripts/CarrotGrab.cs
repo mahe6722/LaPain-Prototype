5b7febae2515b96a083ae87c7f8d90d1d9a55c7a
Unohu <Unohu@users.noreply.github.com>
2019-03-03 17:07:16 +0100

    BegginingsOfBossFight

---
+++ Assets/Scripts/CarrotGrab.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarrotGrab : MonoBehaviour
+{
+    SnekoMovement snekoMovementScript;
+    ExtendableArm extendableArmScript;
+
+
+	void Start ()
+    {
+        snekoMovementScript = GameObject.Find("Sneko").GetComponent<SnekoMovement>();
+        extendableArmScript = GameObject.Find("ExtArm").GetComponent<ExtendableArm>();
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (snekoMovementScript.stageCounter == snekoMovementScript.numberOfStages)
+        {
+            if (other.tag == "ExtArm")
+            {
+                gameObject.transform.parent = null;
+                gameObject.transform.parent = GameObject.Find("ExtArm").transform;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
